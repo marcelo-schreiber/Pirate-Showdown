4f773a2607742dc79b84f643eb51c21f7265de28
marcelo-schreiber <marcelorissette15@gmail.com>
2025-08-28 11:35:45 -0300

    Custom shadows

--- src/models/Ship.tsx
+++ src/models/Ship.tsx
@@ -2,19 +2,35 @@ import * as THREE from "three";
 import { useGLTF } from "@react-three/drei";
 import type { JSX } from "react";
 import type { Mesh } from "three";
+import { useControls } from "leva";
 
 export function ShipModel(props: JSX.IntrinsicElements["group"]) {
   const { nodes, materials } = useGLTF("/Ship-transformed.glb");
   materials.Atlas.side = THREE.DoubleSide;
+
+  const shadowControls = useControls("Shadow", {
+    color: "#ff0000",
+    opacity: 1,
+  });
+
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={(nodes.Ship_Large as Mesh).geometry}
+        scale={100}
+      >
+        <shadowMaterial attach="material" color={shadowControls.color} opacity={shadowControls.opacity} />
+      </mesh>
+            <mesh
+        castShadow
+        receiveShadow
+        geometry={(nodes.Ship_Large as Mesh).geometry}
         material={materials.Atlas}
         scale={100}
-      />
+      >
+      </mesh>
     </group>
   );
 }
