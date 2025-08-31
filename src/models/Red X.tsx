ab435d8a53bd987314096c2b7b75f4632d04990d
marcelo-schreiber <marcelorissette15@gmail.com>
2025-08-31 20:34:06 -0300

    Add Red X model with GLTF support

---
+++ src/models/Red X.tsx
@@ -0,0 +1,19 @@
+import { useGLTF } from "@react-three/drei";
+import type { JSX } from "react";
+import type { Mesh } from "three";
+
+export function RedXModel(props: JSX.IntrinsicElements["group"]) {
+  const { nodes, materials } = useGLTF("/models/Red X.glb");
+  
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        geometry={(nodes.UI_Red_X as Mesh).geometry}
+        material={materials.Atlas}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/Red X.glb");
